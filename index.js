596960ff856b99631f2bc44beb1a8379e017498e
zhSHUVO <zhshuvo99@gmail.com>
2022-04-30 00:11:01 +0600

    server setting with .env

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-//middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9gfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const dressCollection = client.db("fabricHouse").collection("dress");
+        app.get("/dress", async (req, res) => {
+            const query = {};
+            const cursor = dressCollection.find(query);
+            const dresses = await cursor.toArray();
+            res.send(dresses);
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("server is running");
