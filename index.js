bb44833b0ad0018341a2fe3c20047a050a0678af
zhSHUVO <zhshuvo99@gmail.com>
2022-05-04 19:56:24 +0600

    delete single item

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
             );
             res.send(result);
         });
+
+        app.delete("/dress/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dressCollection.deleteOne(query);
+            res.send(result);
+        });
     } finally {
     }
 }
